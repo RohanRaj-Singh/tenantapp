0145d18edf59fdb8a9ee6c412b3c188078b4fd36
RohanRaj-Singh <mrsinghdev13@gmail.com>
2026-05-20 03:32:50 +0500

    feat: remove "Prefer not to say" option from gender selection

--- data/survey.ts
+++ data/survey.ts
@@ -10,5 +10,4 @@ export const genderOptions = [
   { label: "Male", value: "male" },
   { label: "Female", value: "female" },
   { label: "Non-binary", value: "non-binary" },
-  { label: "Prefer not to say", value: "prefer-not-to-say" },
 ];
